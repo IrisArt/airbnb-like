d873cc962970bbbc74463d9d869294c153d72404
RSamaium <sam@newbot.io>
2025-06-18 17:43:21 +0200

    for

---
+++ src/app/features/autocomplete/autocomplete.ts
@@ -0,0 +1,38 @@
+import { Component, input, output, signal } from '@angular/core';
+
+export interface City {
+  name: string
+  region: string
+}
+
+@Component({
+  selector: 'app-autocomplete',
+  imports: [],
+  templateUrl: './autocomplete.html',
+  styleUrl: './autocomplete.css'
+})
+export class Autocomplete {
+  valueSelected = input.required<string>()
+  handleSelectCity = output<City>()
+
+  cities = signal<City[]>([{
+    name: 'Paris',
+    region: 'Île-de-France'
+  }, {
+    name: 'Lyon',
+    region: 'Auvergne-Rhône-Alpes'
+  }, {
+    name: 'Marseille',
+    region: 'Provence-Alpes-Côte d\'Azur'
+  }, {
+    name: 'Toulouse',
+    region: 'Occitanie'
+  }, {
+    name: 'Nice',
+    region: 'Provence-Alpes-Côte d\'Azur'
+  }])
+  
+  selectCity(city: City) {
+    this.handleSelectCity.emit(city)
+  }
+}
--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms'
+import { Autocomplete, City } from '../autocomplete/autocomplete';
 
 @Component({
   selector: 'app-home',
-  imports: [FormsModule],
+  imports: [FormsModule, Autocomplete],
   template: `
     <main class="h-screen flex flex-col items-center justify-center px-4">
       <h1 class="text-5xl font-bold text-center mb-8 text-gray-800">
@@ -12,11 +13,14 @@ import { FormsModule } from '@angular/forms'
       
       <div class="w-full max-w-lg bg-white rounded-lg shadow-lg p-4">
         <div class="flex flex-col sm:flex-row gap-3">
-          <input
-            [(ngModel)]="city"
-            placeholder="Recherche r un logement"
-            class="flex-grow p-3 text-gray-800 rounded-lg border border-gray-300 focus:outline-none focus:border-gray-500"
-          />
+          <div class="relative flex-grow">
+            <input
+              [(ngModel)]="citySelected"
+              placeholder="Recherche un logement"
+              class="w-full p-3 text-gray-800 rounded-lg border border-gray-300 focus:outline-none focus:border-gray-500"
+            />
+            <app-autocomplete [valueSelected]="citySelected()" (handleSelectCity)="listenCityChange($event)" />
+          </div>
           <button
             (click)="search()"
             class="w-full sm:w-auto bg-gray-600 hover:bg-gray-700 p-3 rounded-lg text-white font-medium cursor-pointer"
@@ -29,10 +33,14 @@ import { FormsModule } from '@angular/forms'
   `,
 })
 export class HomeComponent {
-  city = signal('Paris')
- 
+  citySelected = signal('Paris')
+  
   search() {
-    console.log(this.city())
+    console.log(this.citySelected())
+  }
+
+  listenCityChange(city: City) {
+    this.citySelected.set(city.name)
   }
 }
 
