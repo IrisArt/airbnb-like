33f446edff240ca591c360f1b7b28e9230d33985
RSamaium <sam@newbot.io>
2025-06-27 16:06:50 +0200

    Refactor le composant Map pour simplifier la gestion des références et améliorer la lisibilité du code.

--- src/app/features/map/map.ts
+++ src/app/features/map/map.ts
@@ -1,33 +1,37 @@
-import { Component, ElementRef, OnInit, viewChild, viewChildren } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  viewChild,
+  viewChildren,
+} from '@angular/core';
 import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map',
   template: `
     <div #refMap class="w-full h-96 border border-gray-300 rounded-lg"></div>
-    <div #refMap class="w-full h-96 border border-gray-300 rounded-lg"></div>
   `,
 })
 export class MapComponent implements OnInit {
   private map!: L.Map;
- // mapContainer = viewChild<ElementRef<HTMLDivElement>>('refMap');
- mapContainers = viewChildren<ElementRef<HTMLDivElement>>('refMap');
+  mapContainer = viewChild<ElementRef<HTMLDivElement>>('refMap');
+  //mapContainers = viewChildren<ElementRef<HTMLDivElement>>('refMap');
 
   ngOnInit(): void {
-    for (let map of this.mapContainers()) {
-        const el = map?.nativeElement;
-        this.map = L.map(el!).setView([48.8566, 2.3522], 13);
+    //for (let map of this.mapContainers()) {
+    const el = this.mapContainer()?.nativeElement;
+    this.map = L.map(el!).setView([48.8566, 2.3522], 13);
 
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution:
-            '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-        maxZoom: 19,
-        }).addTo(this.map);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      maxZoom: 19,
+    }).addTo(this.map);
 
-        L.marker([48.8566, 2.3522])
-        .addTo(this.map)
-        .bindPopup('<b>Paris, France</b><br/>Capitale de la France')
-        .openPopup();
-    }
+    L.marker([48.8566, 2.3522])
+      .addTo(this.map)
+      .bindPopup('<b>Paris, France</b><br/>Capitale de la France')
+      .openPopup();
   }
 }
