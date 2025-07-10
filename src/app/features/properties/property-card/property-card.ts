430d9e1bdca8797371b6c4338f32fc8b2b3e801f
RSamaium <sam@newbot.io>
2025-07-10 16:29:50 +0200

    Ajout de références de contenu dans le composant PropertyCard et mise à jour du template PropertyList pour intégrer des slots dynamiques.

--- src/app/features/properties/property-card/property-card.ts
+++ src/app/features/properties/property-card/property-card.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, contentChild, contentChildren, effect, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-property-card',
@@ -6,4 +6,12 @@ import { Component } from '@angular/core';
   templateUrl: './property-card.html'
 })
 export class PropertyCard {
+  image = contentChild<ElementRef<HTMLImageElement>>('imageRef')
+  slots = contentChildren<ElementRef<HTMLDivElement>>('slotRef')
+
+  constructor() {
+    effect(() => {
+      console.log(this.slots())
+    })
+  }
 } 
