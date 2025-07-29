2e541c50d5b988889bb8f6b23bbd883870c43c02
RSamaium <sam@newbot.io>
2025-07-29 10:25:34 +0200

    resource

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,26 +1,50 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { Component, inject, OnInit, resource, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Autocomplete, City } from '../autocomplete/autocomplete';
 import { PropertyListComponent } from '../properties/property-list/property-list';
 import { MapComponent } from '../map/map';
-import { PropertiesModel } from '../../core/properties/properties';
+import { PropertiesModel, Property } from '../../core/properties/properties';
 
 @Component({
   selector: 'app-home',
   imports: [FormsModule, Autocomplete, PropertyListComponent, MapComponent],
   templateUrl: './home.html'
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
   private propertiesModel = inject(PropertiesModel)
 
   citySelected = signal('');
   arrivalSelected = signal<Date | null>(null)
   departureSelected = signal<Date | null>(null)
-  properties = this.propertiesModel.properties
 
-  ngOnInit(): void {
-    this.propertiesModel.fetchProperties().subscribe()
-  }
+  searchValue = signal('')
+
+  // properties = resource<Property[], { s: string }>({
+  //   params: () => {
+  //     return {
+  //       s: this.searchValue()
+  //     }
+  //   },
+  //   loader: ({ params, abortSignal }) => {
+  //     return fetch('https://apprendre.angular.fr/api/fake/properties?s=' + params.s, {
+  //       signal: abortSignal
+  //     }).then(res => res.json())
+  //   },
+  //   defaultValue: []
+  // })
+
+  properties = rxResource<Property[], { s: string }>({
+    params: () => {
+      return {
+        s: this.searchValue()
+      }
+    },
+    stream: ({ params }) => {
+      return this.propertiesModel.fetchProperties()
+    },
+    defaultValue: []
+  })
 
   search() {
     console.log('Searching for:', {
