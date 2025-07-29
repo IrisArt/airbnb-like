5cb7d4f3c73ff690be290c0963a2ef9c7b0a74df
RSamaium <sam@newbot.io>
2025-07-29 10:49:17 +0200

    async

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,26 +1,39 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, DestroyRef, inject, OnDestroy, OnInit, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Autocomplete, City } from '../autocomplete/autocomplete';
 import { PropertyListComponent } from '../properties/property-list/property-list';
 import { MapComponent } from '../map/map';
 import { PropertiesModel } from '../../core/properties/properties';
+import { Subscription } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [FormsModule, Autocomplete, PropertyListComponent, MapComponent],
+  imports: [FormsModule, Autocomplete, PropertyListComponent, MapComponent, AsyncPipe],
   templateUrl: './home.html'
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
   private propertiesModel = inject(PropertiesModel)
+  private destroyRef = inject(DestroyRef)
 
   citySelected = signal('');
   arrivalSelected = signal<Date | null>(null)
   departureSelected = signal<Date | null>(null)
-  properties = this.propertiesModel.properties
+  properties$ = this.propertiesModel.fetchProperties()
+  subscription!: Subscription
 
-  ngOnInit(): void {
-    this.propertiesModel.fetchProperties().subscribe()
-  }
+  // constructor() {
+  //   this.propertiesModel.fetchProperties().pipe(
+  //     takeUntilDestroyed()
+  //   ).subscribe()
+  // }
+
+  // ngOnInit(): void {
+  //     this.propertiesModel.fetchProperties().pipe(
+  //       takeUntilDestroyed(this.destroyRef)
+  //     ).subscribe()
+  // }
 
   search() {
     console.log('Searching for:', {
