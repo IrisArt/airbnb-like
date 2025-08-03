f974165b43498c721851ba894825c8ed6785f212
RSamaium <sam@newbot.io>
2025-08-03 15:53:11 +0200

    observable

--- src/app/core/validators/email-exists.ts
+++ src/app/core/validators/email-exists.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { inject } from "@angular/core";
 import { AbstractControl } from "@angular/forms";
-import { map, Observable } from "rxjs";
+import { lastValueFrom, map, Observable, switchMap, timer } from "rxjs";
 
 type EmailExistsData = {
     message: string
@@ -11,10 +11,17 @@ type EmailExistsData = {
 export function emailExistsValidator() {
     const http = inject(HttpClient)
     return function(control: AbstractControl<string>): Observable<{ emailExists: boolean } | null> {
-        return http.post<EmailExistsData>('https://apprendre.angular.fr/api/fake/check-email', {
-            email: control.value
-        }).pipe(
+        return timer(500).pipe(
+            switchMap(() => {
+                return http.post<EmailExistsData>('https://apprendre.angular.fr/api/fake/check-email', {
+                    email: control.value
+                })
+            }),
             map((data) => data.exists ? { emailExists: true } : null)
         )
+        // const data = await lastValueFrom(http.post<EmailExistsData>('https://apprendre.angular.fr/api/fake/check-email', {
+        //     email: control.value
+        // }))
+        // return  data.exists ? { emailExists: true } : null
     }
 }
