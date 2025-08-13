500173a2094ea77c3e154b712f708ef6c3680f18
RSamaium <sam@newbot.io>
2025-08-13 17:00:50 +0200

    service

--- src/app/mytest/mytest.service.spec.ts
+++ src/app/mytest/mytest.service.spec.ts
@@ -1,42 +1,58 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { TestBed } from '@angular/core/testing'
 import { beforeEach, describe, expect, test, vi } from 'vitest'
-import { Mytest } from './mytest'
-import { HttpClient, httpResource } from '@angular/common/http'
-import { resource } from '@angular/core'
 import { MyTestService } from './mytest.service'
-import { provideHttpClientTesting } from '@angular/common/http/testing'
-import { of } from 'rxjs'
+import { Injectable, resource } from '@angular/core'
+
+@Injectable()
+class MyTestServiceMock extends MyTestService {
+  override data: any = resource({
+    loader: async () => {
+      return {
+        properties: [
+          {
+            "id": 1,
+            "title": "Appartement moderne avec vue sur la Seine",
+            "location": "Paris, Île-de-France",
+            "price": 120,
+            "rating": 4.8,
+            "image": "https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?w=400&h=300&fit=crop",
+            "description": "Magnifique appartement de 2 chambres avec vue imprenable sur la Seine. Idéal pour un séjour romantique.",
+            "amenities": [
+              "WiFi",
+              "Cuisine équipée",
+              "Balcon",
+              "Vue sur Seine"
+            ],
+            "maxGuests": 4,
+            "bedrooms": 2,
+            "bathrooms": 1
+          }
+        ]
+      }
+    },
+    defaultValue: {
+        properties: []
+    }
+  })
+}
 
 describe('Tester MyTestComponent', () => {
   let service: MyTestService
-  let fixture
-  let requestSpy = vi.fn();
-  let getSpy = vi.fn();
 
   beforeEach(async () => {
-
-    const body = [{ id: 1, name: 'Ada' }];
-    getSpy.mockReturnValue(of(body));
-
     await TestBed.configureTestingModule({
-      providers: [MyTestService, {
-        provide: HttpClient,
-        useValue: {
-            // httpResource passe par `request`. On mocke les deux pour sécurité.
-            request: requestSpy,
-            get: getSpy,
-          },
+      providers: [{
+        provide: MyTestService,
+        useClass: MyTestServiceMock
       }]
     })
     
     service = TestBed.inject(MyTestService)
-
+    TestBed.tick()
   })
 
-  test('tester data', async () => {
-   
+  test('tester data', () => {
     const data = service.data.value()
-    console.log(data)
-    expect(data.properties.length).toBe(4)
+    expect(data.properties.length).toBe(1)
   })
 })
